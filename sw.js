2665815fcd4bf1359a5276e1a5212d0e02922808
Ercsion <bhs5612@gmail.com>
2026-07-29 12:51:25 +0800

    Add 58 questions from Official Pretest (user-collected content)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline cache for the AIF-C01 study app (PWA).
-var CACHE_NAME = "aif-app-cache-v4";
+var CACHE_NAME = "aif-app-cache-v5";
 var ASSETS = [
   "./index.html",
   "./style.css",
